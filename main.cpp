b6cf215208cfd090a06c9980123f89fb96b9d45b
Kyle <djprophecy21@gmail.com>
2025-04-06 02:08:42 -0500

    got it working really nice. can't wait to add more to it now. no bugs so far

--- main.cpp
+++ main.cpp
@@ -1,24 +1,25 @@
 #include <iostream>
+#include <thread>
+#include <chrono>
 #include "player.h"
 #include "enemy.h"
 #include "utils.h"
 
-
 using namespace std;
 
-/// @brief 
-/// @return 
 int main() {
     // Initialize the game
     Player player = createPlayer();
     showIntro();
-    
-    while (player.health > 0) {
-       enterRoom(player);
-       // gameloop here.
-    };
+    this_thread::sleep_for(chrono::seconds(2)); // Pause after intro
 
-    cout << "Game Over! You have been defeated,\n" << player.name << "!\n";
+    while (player.health > 0) {
+        enterRoom(player);
+        this_thread::sleep_for(chrono::seconds(2)); // Pause between rooms
+    }
+
+    cout << "Game Over! You have been defeated, " << player.name << "!\n";
+    this_thread::sleep_for(chrono::seconds(2)); // Pause before final message
     cout << "Thanks for playing!\n";
 
     return 0;
--- utils.cpp
+++ utils.cpp
@@ -1,5 +1,7 @@
 #include "utils.h"
 #include <iostream>
+#include <thread>
+#include <chrono>
 #include "player.h"
 
 using namespace std;
@@ -9,11 +11,16 @@ void showIntro()
     cout << "Welcome to the Text RPG!\n";
     cout << "You are a brave adventurer on a quest to find treasure.\n";
     cout << "Your journey begins now!\n";
-};
+    this_thread::sleep_for(chrono::seconds(2)); // Pause after intro text
+}
 
 void enterRoom(Player &player)
 {
     cout << "You have entered a mysterious room....\n";
+    this_thread::sleep_for(chrono::seconds(1)); // Pause for suspense
     displayStats(player);
-    cout << "Nothing happens...this time.\n";
-};
+    player.health -= 10; // Simulate taking damage
+    cout << "You took 10 damage!\n";
+    this_thread::sleep_for(chrono::seconds(1)); // Pause after damage
+    displayStats(player);
+}
